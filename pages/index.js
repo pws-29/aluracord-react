415cfac01761d5c90aaaf144104a7017f560e8b5
Pietro Weg <pietrows29@gmail.com>
2022-01-24 12:56:40 -0300

    Commit Inicial

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+import GlobalStyles from './GlobalStyles.js'
+
+function HomePage() {
+  return (
+    <div>
+      <GlobalStyles />
+      <h1>Boas vindas de volta!</h1>
+      <h2>Discord - Alura Matrix</h2>
+    </div>
+  );
+};
+
+
+export default HomePage
